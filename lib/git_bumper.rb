1870613e64d627f2c6bae310c3cf10e3db6cc802
Lenon Marcel <lenon.marcel@gmail.com>
2015-12-12 01:52:54 -0200

    Add a glue method to parse and run the CLI

--- lib/git_bumper.rb
+++ lib/git_bumper.rb
@@ -2,6 +2,15 @@ require 'git_bumper/version'
 require 'git_bumper/tag'
 require 'git_bumper/build_tag'
 require 'git_bumper/git'
+require 'git_bumper/cli_parser'
+require 'git_bumper/cli'
 
 module GitBumper
+  module_function
+
+  def run
+    parser = CLIParser.new(ARGV)
+    parser.parse
+    CLI.new(parser.options).run
+  end
 end
